471cdfbccf9c3b41f8c93f0eeab19a5d5f67e081
Darius Costolas <dariuscostolas@yahoo.com>
2018-06-27 14:33:30 +0300

    Add events

--- main.js
+++ main.js
@@ -1,77 +1,126 @@
 import Web3 from 'web3';
-import { soliditySha3 } from 'web3-utils';
+import {
+    soliditySha3
+} from 'web3-utils';
 
 let web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 
-console.log(soliditySha3(512, web3.eth.accounts[0]))
-console.log(web3.eth.accounts[0]);
 window.soliditySha3 = soliditySha3;
 web3.eth.defaultAccount = web3.eth.accounts[0];
 const GreeterContract = web3.eth.contract([{
-    "constant": false,
-    "inputs": [{
-        "name": "number",
-        "type": "uint16"
-    }],
-    "name": "buyTicket",
-    "outputs": [],
-    "payable": true,
-    "stateMutability": "payable",
-    "type": "function"
-}, {
-    "constant": true,
-    "inputs": [{
-        "name": "",
-        "type": "int256"
-    }, {
-        "name": "",
-        "type": "uint256"
-    }],
-    "name": "numbers",
-    "outputs": [{
-        "name": "",
-        "type": "address"
-    }],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-}, {
-    "constant": true,
-    "inputs": [{
-        "name": "number",
-        "type": "uint16"
-    }],
-    "name": "numberBoughtCount",
-    "outputs": [{
-        "name": "",
-        "type": "uint256"
-    }],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-}, {
-    "constant": true,
-    "inputs": [],
-    "name": "owner",
-    "outputs": [{
-        "name": "",
-        "type": "address"
-    }],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-}, {
-    "inputs": [],
-    "payable": false,
-    "stateMutability": "nonpayable",
-    "type": "constructor"
-}]);
-const Greeter = GreeterContract.at('0xd7944fc8e63aeb974d540b67211349c987d12f17');
+        "constant": false,
+        "inputs": [{
+            "name": "ticketNumber",
+            "type": "uint256"
+        }],
+        "name": "enterTicket",
+        "outputs": [],
+        "payable": true,
+        "stateMutability": "payable",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [{
+            "name": "k",
+            "type": "uint256"
+        }],
+        "name": "test",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    },
+    {
+        "constant": false,
+        "inputs": [{
+            "name": "number",
+            "type": "uint256"
+        }],
+        "name": "yas",
+        "outputs": [{
+            "name": "",
+            "type": "bytes32"
+        }],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "anonymous": false,
+        "inputs": [{
+            "indexed": false,
+            "name": "hash",
+            "type": "bytes32"
+        }],
+        "name": "YasEmitted",
+        "type": "event"
+    },
+    {
+        "constant": true,
+        "inputs": [{
+            "name": "",
+            "type": "uint256"
+        }],
+        "name": "tArr",
+        "outputs": [{
+            "name": "",
+            "type": "uint256"
+        }],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "test2",
+        "outputs": [{
+            "name": "",
+            "type": "uint256"
+        }],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [{
+                "name": "",
+                "type": "uint256"
+            },
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "name": "ticketNumberToPlayers",
+        "outputs": [{
+            "name": "",
+            "type": "address"
+        }],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    }
+]);
+const Greeter = GreeterContract.at('0xa25498a51f770708d6697951630732354907e2a7');
 
-Greeter.buyTicket(2, {
+Greeter.enterTicket(52, {
     gas: 300000,
     from: web3.eth.accounts[0],
-    value: web3.toWei(0.1, 'ether')
+    value: web3.toWei(0.01, 'ether')
 }, (err, result) => {
     console.log(err, result)
 })
+
+// const event = Greeter.YasEmitted();
+// event.watch((e, r) => {
+//     console.log(e, r);
+// })
