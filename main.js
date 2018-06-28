0c8b3ba40100b95ad6c4a026f7141327d475fd5d
Darius Costolas <dariuscostolas@yahoo.com>
2018-06-28 12:33:34 +0300

    Move abi to different file

--- main.js
+++ main.js
@@ -2,124 +2,29 @@ import Web3 from 'web3';
 import {
     soliditySha3
 } from 'web3-utils';
+const abi = require('./abi.json');
 
 let web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 
 window.soliditySha3 = soliditySha3;
 web3.eth.defaultAccount = web3.eth.accounts[0];
-const GreeterContract = web3.eth.contract([{
-        "constant": false,
-        "inputs": [{
-            "name": "ticketNumber",
-            "type": "uint256"
-        }],
-        "name": "enterTicket",
-        "outputs": [],
-        "payable": true,
-        "stateMutability": "payable",
-        "type": "function"
-    },
-    {
-        "constant": false,
-        "inputs": [{
-            "name": "k",
-            "type": "uint256"
-        }],
-        "name": "test",
-        "outputs": [],
-        "payable": false,
-        "stateMutability": "nonpayable",
-        "type": "function"
-    },
-    {
-        "inputs": [],
-        "payable": false,
-        "stateMutability": "nonpayable",
-        "type": "constructor"
-    },
-    {
-        "constant": false,
-        "inputs": [{
-            "name": "number",
-            "type": "uint256"
-        }],
-        "name": "yas",
-        "outputs": [{
-            "name": "",
-            "type": "bytes32"
-        }],
-        "payable": false,
-        "stateMutability": "nonpayable",
-        "type": "function"
-    },
-    {
-        "anonymous": false,
-        "inputs": [{
-            "indexed": false,
-            "name": "hash",
-            "type": "bytes32"
-        }],
-        "name": "YasEmitted",
-        "type": "event"
-    },
-    {
-        "constant": true,
-        "inputs": [{
-            "name": "",
-            "type": "uint256"
-        }],
-        "name": "tArr",
-        "outputs": [{
-            "name": "",
-            "type": "uint256"
-        }],
-        "payable": false,
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "constant": true,
-        "inputs": [],
-        "name": "test2",
-        "outputs": [{
-            "name": "",
-            "type": "uint256"
-        }],
-        "payable": false,
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "constant": true,
-        "inputs": [{
-                "name": "",
-                "type": "uint256"
-            },
-            {
-                "name": "",
-                "type": "uint256"
-            }
-        ],
-        "name": "ticketNumberToPlayers",
-        "outputs": [{
-            "name": "",
-            "type": "address"
-        }],
-        "payable": false,
-        "stateMutability": "view",
-        "type": "function"
-    }
-]);
-const Greeter = GreeterContract.at('0xa25498a51f770708d6697951630732354907e2a7');
+const GreeterContract = web3.eth.contract(abi);
+// web3.eth.subscribe('pendingTransactions', function (error, result) {})
+//     .on("data", function (trxData) {
+//         web3.eth.getTransaction(trxData).then(console.log);
+//     });
+const Greeter = GreeterContract.at('0x66e4be6573ab28a60d6df109036596ef146ba099');
 
-Greeter.enterTicket(52, {
-    gas: 300000,
-    from: web3.eth.accounts[0],
-    value: web3.toWei(0.01, 'ether')
-}, (err, result) => {
-    console.log(err, result)
+Greeter.runLottery.call((err, result) => {
+    console.log(err, result.toNumber())
 })
-
+// Greeter.enterTicket(52, {
+//     gas: 300000,
+//     from: web3.eth.accounts[0],
+//     value: web3.toWei(0.01, 'ether')
+// }, (err, result) => {
+//     console.log(err, result)
+// })
 // const event = Greeter.YasEmitted();
 // event.watch((e, r) => {
 //     console.log(e, r);
